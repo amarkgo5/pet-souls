34c4093bd34d3e173dd6e20613301b75df77c022
cpollack <c_pollack@hotmail.com>
2017-09-29 15:26:07 -0400

    Button and New Scene

---
+++ btnExplore.gd
@@ -0,0 +1,23 @@
+extends Button
+
+# class member variables go here, for example:
+#signal pressed
+var on = false
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _draw():
+	var r = Rect2(Vector2(), get_size())
+	if (on):
+		draw_rect(r, Color(1,0,0))
+	else:
+		draw_rect(r, Color(0,1,0))
+		
+func _input_event(event):
+	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed):
+		on = true
+		emit_signal("pressed")
+		update()
---
+++ world1.gd
@@ -0,0 +1,19 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var btnExplore
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	btnExplore = get_node("btnExplore")
+	btnExplore.connect("pressed", self, "_on_btnExplore_pressed")
+	pass
+
+func _on_btnExplore_pressed():
+	# create instance of battle
+	var scene = load("res://Battle.tscn")
+	var node = scene.instance()
+	add_child(node)
