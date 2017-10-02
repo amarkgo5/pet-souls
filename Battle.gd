9b3d716b32a89e821585851d76f5e4f5446f6e6b
cpollack <c_pollack@hotmail.com>
2017-10-02 17:02:42 -0400

    Dyanmic Monsters
    Monsters now have their own scene and are dyanmically added by script at the start of the battle.

--- Battle.gd
+++ Battle.gd
@@ -1,13 +1,29 @@
 extends Node
+export (PackedScene) var monster
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
+#handles by group?
+#var monsters = []
+
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	# add three monsters
+	var m1 = monster.instance()
+	m1.setType("monster")
+	m1.coordinates = Vector2(630,240)
+	add_child(m1)
+	
+	var m2 = monster.instance()
+	m2.setType("monster")
+	m2.coordinates = Vector2(750,315)
+	add_child(m2)
+	
+	var m3 = monster.instance()
+	m3.setType("monster")
+	m3.coordinates = Vector2(650,390)
+	add_child(m3)
 	
 func _on_btnExit_pressed():
         get_node("/root/global").goto_scene("res://world1.tscn")
---
+++ monster.gd
@@ -0,0 +1,27 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var name = ""
+var attack = 5
+var defence = 0
+var health = 10
+var coordinates = Vector2(0,0)
+var image = null
+
+#func _init():
+	#if (typeof(pos) == TYPE_VECTOR2): coordinates = pos
+	
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_pos(coordinates)
+	get_node("monsterTexture").set_texture(image)
+	add_to_group("monsters")
+	
+func setType(type):
+	if (type == "" || type == "monster"): image = ResourceLoader.load("res://textures/monsters/monster.png")
+	
