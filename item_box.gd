8d64e8992d499a50844b232d53deed4e2a5df5e1
cpollack <c_pollack@hotmail.com>
2017-11-03 15:16:08 -0400

    logic for mouseover display of item description

--- item_box.gd
+++ item_box.gd
@@ -2,9 +2,12 @@ extends Control
 
 var item_count = 0
 var item_id = null
+var name = ""
+var description = ""
 
 func _ready():
 	set_item_count(item_count)
+	set_process_input(true)
 
 func get_item_id():
 	return item_id
@@ -24,3 +27,16 @@ func set_item_count(count):
 	else:
 		display = ""
 	get_node("boxTexture/item_count").set_text(display)
+
+func _input(event):
+	if event.type == 2:
+		var mouse_event_pos = event.pos
+		var myPos = get_global_pos()
+		if myPos.x < mouse_event_pos.x and mouse_event_pos.x < (myPos.x+50):
+			if myPos.y < mouse_event_pos.y and mouse_event_pos.y < (myPos.y+50):
+				get_node("descPanel").set_hidden(false)
+				get_node("descPanel").set_global_pos(mouse_event_pos)
+			else:
+				get_node("descPanel").set_hidden(true)
+		else:
+			get_node("descPanel").set_hidden(true)
